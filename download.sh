676fdc6dd3ecc2fa4cfe3a32df6cf775cf9c7415
JumperLuko <jumper.luko@gmail.com>
2022-05-13 01:03:31 -0300

    codec official fix

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "More in https://github.com/alvr-org/ALVR/releases"
+echo "Downloading 1.18.2"
+read -p "Enter to continue"
+
+wget https://github.com/alvr-org/ALVR/releases/download/v18.2.3/alvr_server_linux_portable.tar.gz
